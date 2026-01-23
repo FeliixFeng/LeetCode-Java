22e25edc88f31fa2ebd867299598d3bcb26e5e60
feliixfeng <3288733739@qq.com>
2026-01-23 18:17:24 +0800

    P3_LongestSubstringWithoutRepeatingCharacters

---
+++ src/main/java/leetcode/editor/cn/P3_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,27 @@
+package leetcode.editor.cn; 
+
+import java.util.*;
+import leetcode.editor.common.*;
+
+/**
+ * [3] 无重复字符的最长子串
+ */
+public class P3_LongestSubstringWithoutRepeatingCharacters {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+    public static void main(String[] args) {
+        // 实例化外部类以调用内部类 Solution
+        P3_LongestSubstringWithoutRepeatingCharacters outer = new P3_LongestSubstringWithoutRepeatingCharacters();
+        Solution solution = outer.new Solution();
+        
+        System.out.println("本地测试已就绪");
+    }
+}
